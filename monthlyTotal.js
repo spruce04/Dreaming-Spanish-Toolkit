09ebcf9ffb905f551c793a7f73bcb1a1613214f4
Sam Flax <103819850+spruce04@users.noreply.github.com>
2023-06-18 17:53:49 +1000

    Update and rename monthlyTotal.js to monthlyStats.js

--- monthlyTotal.js
+++
@@ -1,9 +0,0 @@
-const days = document.getElementsByClassName("ds-form-calendar__column-time ds-form-calendar__column-time--emphasized");
-let hoursWatched = 0;
-for(let i = 0; i < days.length; i++) {
-    let dailyTime = parseInt(days[i].textContent.slice(0, days[i].textContent.length - 1));
-    hoursWatched += dailyTime;
-};
-let hourCount = parseInt(hoursWatched/60);
-minuteCount = hoursWatched - 60 * hourCount;
-console.log(`Total watched this month: ${hourCount} hours and ${minuteCount} minutes`);
